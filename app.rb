1ecbf1f5fac9fb3786c74a8d92fbb74d22e10ff2
Jamie Patel <jamie@notespublication.com>
2014-10-29 14:17:45 +0000

    Changed css link in head

--- app.rb
+++ app.rb
@@ -3,16 +3,13 @@ require 'sinatra'
 require 'haml'
 require 'json'
 require "gravatar-ultimate"
-
-
-# Helpers
 require './lib/render_partial'
-
 require 'pusher'
+require 'mongo'
+
 
 Pusher.url = "http://5b38b811cbe170b81ea1:658c86a2384410f3e45c@api.pusherapp.com/apps/94047"
 
-require 'mongo'
 
 include Mongo
 
@@ -68,17 +65,3 @@ def match_people_to_mac_addresses addresses
 	matches.to_a.each { |match| people.update({"_id" => match["_id"]},{"$set" => {"last_seen" => Time.now}})}
 	people.find('mac' => {'$in' => addresses}).to_a
 end
-
-# def in_groups array, odd_number, even_number
-# 	number = odd_number
-# 	array = array.each_slice(even_number).to_a
-# 	array.each_with_index do |sub_array, index|
-# 		puts number
-# 		until sub_array.length <= number
-# 			array[index + 1] = [] if !array[index + 1]
-# 			array[index + 1] << sub_array.pop
-# 		end
-# 		number = (number == odd_number) ? even_number : odd_number
-# 	end
-# 	return array
-# end
